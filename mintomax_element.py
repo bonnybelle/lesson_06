4a06a1a0bd9480a8f76d261494e107bacf6b51ca
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2019-10-20 15:20:07 +0300

    Update mintomax_element.py

--- mintomax_element.py
+++ mintomax_element.py
@@ -1,12 +1,14 @@
 # 10. В списке все элементы различны. Поменяйте местами минимальный и максимальный элемент этого списка.
 # Создавать новый список недопустимо.
 
-a = [int(i) for i in input('Введите числа: ').split()]
-x = min(a)
-y = max(a)
-for i in range(len(a)):
-    for j in range(len(a)):
-        if a[i] == y and a[j] == x:
-            a[i] = x
-            a[j] = y
+from random import randint
+
+a = [randint(1, 50) for i in range(10)]
+print(a)
+for x in range(len(a)):
+    for y in range(len(a)):
+        if a[x] == max(a) and a[y] == min(a):
+            i = a[x]
+            a[x] = min(a)
+            a[y] = i
 print(a)
